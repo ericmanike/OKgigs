5475190b211fc66a66d41bda3dc2bf56b5aa15ee
Ericsson <manikeeric@gmail.com>
2026-02-21 03:27:30 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { ArrowRight, AlertCircle, Shield } from "lucide-react";
 import { motion } from "motion/react";
 import { FaPhone, FaWhatsapp } from "react-icons/fa";
-import Pattern from "@/components/ui/bgParttern";
+import Pattern from "@/components/ui/Parttern";
 
 export default function Home() {
   return (
