486be85dffac96500b92d097a08a8b0a487ffde6
Ericsson <manikeeric@gmail.com>
2026-03-06 10:36:57 +0000

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://megagigs.online'
+    const baseUrl = 'https://megagigs.vercel.app'
 
     return [
         {
